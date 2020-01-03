2faf75eb6a3370852217f5b179242fa898555833
curlyhilfiger <evseev.arw@gmail.com>
2020-01-03 18:20:39 +0300

    All excess deleted

--- app/data_handler.py
+++
@@ -1,8 +0,0 @@
-import requests 
-
-def get_character():
-    r = requests.get('https://swapi.co/api/people/')
-    data = str(r.json())
-    print(data) 
-
-get_character()
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-from flask import render_template, request, redirect
+from flask import render_template
 
 from app import app
 
@@ -19,7 +19,6 @@ def characters():
         data = json.loads(json_str)
         res.append(data['results'])
         url = data['next']
-    print(res)
     return render_template(
         "public/templates/public_template.html",
         characters=res,
@@ -47,49 +46,15 @@ def planets():
 def starships():
     """ Starships view page """
     current = 'Starships'
-    with app.open_resource('static/txts/starships.txt') as txt:
-        json_str = txt.read()
+    url = 'https://swapi.co/api/starships/'
+    res = []
+    while url:
+        req = requests.get(url)
+        json_str = req.text
         data = json.loads(json_str)
+        res.append(data['results'])
+        url = data['next']
     return render_template(
-        "public/templates/starships.html",
-        starships=data,
+        "public/templates/starships.html", starships=res,
         current=current
     )
-
-@app.route("/sign_up", methods=["GET", "POST"])
-def sign_up():
-    """ Sign up page (remove lately) """
-    if request.method == "POST":
-        req = request.form
-
-        print(req)
-        username = req["username"]
-        email = req.get("email")
-        password = request.form["password"]
-        print(username, email, password)
-
-        return redirect(request.url)
-
-    return render_template("public/templates/sign_up.html")
-
-@app.route("/search", methods=["GET", "POST"])
-def search():
-    """ Search data in json txt """
-    if request.method == "POST":
-        req = request.form
-        search_str = req["search"]
-        print(search_str)
-        with app.open_resource('static/txts/characters.txt') as txt:
-            json_str = txt.read()
-            data = json.loads(json_str) 
-            for d in data:
-                if search_str == d["name"]:
-                    print(d)
-                    return render_template("public/templates/public_template.html", 
-                                           characters=d,
-                                           current='Characters'
-                                           )
-                return render_template("public/templates/public_template.html", error="Not Found")
-        return redirect(request.url)
-
-    return render_template("public/templates/public_template.html")
